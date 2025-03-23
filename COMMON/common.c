2285ac0c73fd27fc70102ecdd639f6f1b5fa54a5
asw3005 <asw3005@gmail.com>
2025-03-23 20:44:22 +0300

    qwert

--- COMMON/common.c
+++ COMMON/common.c
@@ -1,153 +1,396 @@
 /*
- * @brief Set of common data and functions.
- * 
- * Created 08.19.21
+ * @brief Common.c
  *
- **/
+ * Created on: Jul 16, 2023
+ * Author: asw3005
+ *
+ */
+#include "stm32f4xx_hal.h"
+#include "stdio.h"
+#include "main.h"
+#include "Common.h"
+#include "DataExchCAN.h"
+#include "CRCCalc.h"
+#include "string.h"
 
-#include "stm32f103xb.h"
 #include "FreeRTOS.h"
-#include "spi.h"
-#include "common.h"
+#include "queue.h"
 #include "semphr.h"
 
 /* External variables. */
-extern SPI_HandleTypeDef hspi1;
 extern I2C_HandleTypeDef hi2c1;
-SemaphoreHandle_t SWBtnPressed_SH;
-//extern CAN_HandleTypeDef hcan;
+extern QueueHandle_t CanDataTx_qh;
+extern SemaphoreHandle_t SoftBtnPressed_sh;
+
+/* Private variables. */
+static I2C_HandleTypeDef* ExtMemI2C = &hi2c1;
+
+static M24128_MEM_DATA_t UserData = {
+	.BYTE_ADDR = GEN_DATA_BYTE,
+	.PAGE_ADDR = GEN_DATA_PAGE
+};
 
 /*
- * @brief Set-up 16-bit list mode identifier to specific bank.
- * 
- * @param hcan : Pointer to a CAN_HandleTypeDef structure that contains
- *        the configuration information for the specified CAN.
- * @param bank : Specifies the filter bank which will be initialized. This parameter must be a number between Min_Data = 0
- *        and Max_Data = 13.
- * @param address : Device address that you wish from 0x00 to 0xFF.
- * @param cmd_x : These four command may be enything what you wish, but you can use this one from the HVU_NarrowCommand or
- *        HVU_BroadcastCommand enums.
- *
- **/
-void CAN_ConfigAddr(CAN_HandleTypeDef *hcan, const uint8_t bank, const uint8_t address, const uint8_t cmd_1,
-					const uint8_t cmd_2,  const uint8_t cmd_3,  const uint8_t cmd_4)
-{
-	CAN_FilterBankRegOrg16_t CanRegOrg16;	
-	CMD_DevId_t DevId;
-	
-	DevId.ID_ADDRESS = address;
-	DevId.RESERVED15_11 = 0;
-	
-	/* Activate initialization mode. */
-	hcan->Instance->FMR |= CAN_FMR_FINIT;
-	
-	/* Set-up identifiers. */
-	CanRegOrg16.IDE = 0;
-	CanRegOrg16.RTR = 0;
-	CanRegOrg16.EXID18_16 = 0;
-	DevId.ID_TODO = cmd_1;
-	CanRegOrg16.STID31_21 = DevId.Identifier;
-	hcan->Instance->sFilterRegister[bank].FR1 = (uint32_t)CanRegOrg16.FilterBankReg;
-	DevId.ID_TODO = cmd_2;
-	CanRegOrg16.STID31_21 = DevId.Identifier;
-	hcan->Instance->sFilterRegister[bank].FR1 |= (uint32_t)CanRegOrg16.FilterBankReg << 16U;
-	DevId.ID_TODO = cmd_3;
-	CanRegOrg16.STID31_21 = DevId.Identifier;
-	hcan->Instance->sFilterRegister[bank].FR2 = (uint32_t)CanRegOrg16.FilterBankReg;
-	DevId.ID_TODO = cmd_4;
-	CanRegOrg16.STID31_21 = DevId.Identifier;
-	hcan->Instance->sFilterRegister[bank].FR2 |= (uint32_t)CanRegOrg16.FilterBankReg << 16U;
-	
-	/* Deactivate initialization mode. */
-	hcan->Instance->FMR &= ~CAN_FMR_FINIT;
+ * @brief Get command codes.
+ */
+
+
+/*
+ * @brief Make command code for each of the text command.
+ */
+uint32_t* MCS_MakeCmdCodes(void) {
+
+	//uint32_t size;
+	/* List of commands. */
+	static char* const CmdList[] = {
+										/* Hardware reading request. */
+										"MCS_HardwareReq",
+										/* Command code to connect to the PCB. */
+										"MCS_ConnectTo",
+										/* MCS command list. */
+										"MCS_GETSysInfo",
+										"MCS_GETDeviceList",
+										"MCS_SETEthAddr",
+										"MCS_SETAddr",
+										"MCS_ENBip",
+										"MCS_DISBip",
+										"MCS_GetParams",
+										"MCS_SetParams",
+										"MCS_SaveParams",
+										"MCS_RestoreParams",
+										"MCS_SyncDevices",
+										/* FPGA FIFO command list. */
+										"FPGA_FifoReset",
+										"FPGA_GetSampleNumbers",
+										"FPGA_GetFifo0samples",
+										"FPGA_GetFifo1samples",
+										"FPGA_GetTSampleNumbers",
+										"FPGA_GetTSamples",
+										/* Configuration commands. */
+										"FPGA_SoftwareStart",
+										"FPGA_SyncRegisters",
+										"FPGA_SetDefaulParams",
+										"FPGA_SetCtrlReg",
+										"FPGA_SetPeriodLimiter",
+										"FPGA_SetFStartTim",
+										"FPGA_SetSStartTim",
+										"FPGA_SetFStopTim",
+										"FPGA_SetSStopTim",
+										"FPGA_SetThrStartHigh",
+										"FPGA_SetThrStartLow",
+										"FPGA_SetThr0Stop",
+										"FPGA_SetThr1Stop",
+										/* Global command set. */
+										"GCS_EnDisDevice",
+										"GCS_VIEWAddr",
+										/* Broadcast command set. */
+										"BCS_EnDisDevice",
+										"BCS_VIEWAddr",
+										/* Unicast command set. */
+										"UCS_GETInfo",
+										"UCS_EnDisDevice",
+										"UCS_SETAddr",
+										"UCS_LOCKAddr",
+										/* Command prefixes. */
+										"FPGA",
+										"MCS",
+										"GCS",
+										"BCS",
+										"UCS",
+										"AGU"
+									};
+
+	static uint32_t CommandCode[sizeof(CmdList)/sizeof(char*)];
+
+	//size = sizeof(CmdList);
+	//size = sizeof(CmdList)/sizeof(char*);
+	//size = strlen(CmdList[0]);
+
+	for(uint8_t i = 0; i < sizeof(CmdList)/sizeof(char*); i++) {
+		CommandCode[i] = Hard_CRC32ETH((uint8_t*)CmdList[i], strlen(CmdList[i]));
+	}
+	__NOP();
+
+	return CommandCode;
 }
 
 /*
- * @brief
- * 
- * @param hcan : pointer to a CAN_HandleTypeDef structure that contains
- *        the configuration information for the specified CAN.
+ * @brief Read all generators' data from external memory on the board.
  *
- **/
-void CAN_ConfigBank(CAN_HandleTypeDef *hcan)
-{
-	/* Activate initialization mode. */
-	hcan->Instance->FMR |= CAN_FMR_FINIT;
-	
-	/* These bits are not matter because the MCU has only one CAN interface. Default value is 14. */
-	hcan->Instance->FMR |= 14 << CAN_FMR_CAN2SB_Pos;
-	/* Enable list mode for the bank 0 to 13. By defalt is identifier mask mode. */
-	hcan->Instance->FM1R = CAN_FM1R_FBM;
-	/* Filter scale is 16 bit value. By default the banks are 16 bit scale configuration. */
-	hcan->Instance->FS1R = 0;
-	/* FIFO assignemt. Bank zero, one in FIFO0, all remaining banks in the FIFO1. */
-	hcan->Instance->FFA1R = (CAN_FFA1R_FFA & ~( CAN_FFA1R_FFA0 | CAN_FFA1R_FFA1));
-	/* Activate specific filterbanks. */	
-	hcan->Instance->FA1R |= CAN_FA1R_FACT0 | CAN_FA1R_FACT1 | CAN_FA1R_FACT2 | CAN_FA1R_FACT3;
-	/* Can interrupt configuration. */
-	hcan->Instance->IER = CAN_IER_TMEIE | CAN_IER_FMPIE0 | CAN_IER_FMPIE1 | CAN_IER_ERRIE;
-	/* Set-up identifiers. */
-	hcan->Instance->sFilterRegister[0].FR1 = 0x00010000;
-	hcan->Instance->sFilterRegister[0].FR2 = 0x00030002;
-	hcan->Instance->sFilterRegister[1].FR1 = 0x00050004;
-	hcan->Instance->sFilterRegister[1].FR2 = 0x00070006;
-	
-	/* Deactivate initialization mode. */
-	hcan->Instance->FMR &= ~CAN_FMR_FINIT;
+ * @param ReadWrite : 0 to write, 1 to read.
+ */
+void DEV_RWGenEepAll(uint8_t ReadWrite) {
+
+	UserData.BYTE_ADDR = GEN_DATA_BYTE;
+	UserData.PAGE_ADDR = GEN_DATA_PAGE;
+	if (!ReadWrite) {
+	/* Read 48 bytes of generators' data per channel. */
+//	for (uint8_t i = 0; i < MAX_SGU_CHANNELS; i++) {
+//		UserData.PAGE_ADDR = GEN_DATA_PAGE + i*3;
+//		HAL_I2C_Mem_Read(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Phase[0],
+//			sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//		UserData.PAGE_ADDR = GEN_DATA_PAGE + 1 + i*3;
+//		HAL_I2C_Mem_Read(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Amplitude[0],
+//			sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//		UserData.PAGE_ADDR = GEN_DATA_PAGE + 2 + i*3;
+//		HAL_I2C_Mem_Read(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Frequency[0],
+//			sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//		}
+//
+//	} else {
+//		/* Write 48 bytes of generators' data per channel. */
+//		for (uint8_t i = 0; i < MAX_SGU_CHANNELS; i++) {
+//			UserData.PAGE_ADDR = GEN_DATA_PAGE + i*3;
+//			HAL_I2C_Mem_Write(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Phase[0],
+//				sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//			UserData.PAGE_ADDR = GEN_DATA_PAGE + 1 + i*3;
+//			HAL_I2C_Mem_Write(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Amplitude[0],
+//				sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//			UserData.PAGE_ADDR = GEN_DATA_PAGE + 2 + i*3;
+//			HAL_I2C_Mem_Write(ExtMemI2C, EEP_DATA_ADDR_SHIFTED, UserData.MemAddr, I2C_MEMADD_SIZE_16BIT, (uint8_t*)&DEV_GetGenData()[i].Frequency[0],
+//				sizeof(DEV_GenData_t)*MAX_UPCB_NUMBER, 25);
+//		}
+	}
 }
 
 /*
- *@brief Software button trigger.
- *
+ * @brief Getter for DEV_TypeList_t struct.
+ */
+DEV_List_t* DEV_GetType(void) {
+
+	static DEV_List_t DevList = { 0 };
+	return &DevList;
+}
+
+/*
+ * @brief Device available list.
+ */
+void DEV_WriteDevType(uint8_t dev_channel, uint32_t dev_type, uint8_t dev_subtype) {
+
+	if ((dev_channel) <= MAX_UPCB_NUMBER) {
+		DEV_GetType()->DevType[dev_channel - 1] = dev_type;
+		DEV_GetType()->DevSubType[dev_channel - 1] = dev_subtype;
+	}
+}
+
+/*
+ * @brief Update the list of available device .
+ */
+void DEV_UpdateState(uint32_t dev_type, uint32_t sprefix, uint8_t address) {
+
+	CAN_TxQueueData_t CAN_TxData = { 0 };
+
+	CAN_TxData.DevType = dev_type;
+	CAN_TxData.SPrefixCode = sprefix;
+
+	/* Clear the list of devices. */
+	HAL_Delay(100);
+	DEV_WriteDevType(address, NO_DEVICE_AVAILABLE, NO_DEVICE_AVAILABLE);
+	CAN_TxData.Address = address;
+	CAN_TxData.Command = UCS_DEV_LINK;
+	xQueueSendToBack(CanDataTx_qh, &CAN_TxData, 0);
+	HAL_Delay(75);
+}
+
+/*
+ * @brief Update the list of available devices.
+ */
+void DEV_UpdateList() {
+
+	CAN_TxQueueData_t CAN_TxData = { 0 };
+
+	CAN_TxData.DevType		= TYPE_UNDEFINED;
+	CAN_TxData.SPrefixCode	= UCS;
+
+	/* Waiting module available. */
+	HAL_Delay(50);
+
+	/* Clear list of available devices. */
+	for (uint8_t i = 1; i <= MAX_UPCB_NUMBER; i++) {
+		DEV_WriteDevType(i, NO_DEVICE_AVAILABLE, NO_DEVICE_AVAILABLE);
+	}
+
+	/*  */
+	for (uint8_t i = 1; i <= MAX_UPCB_NUMBER; i++) {
+		CAN_TxData.Address = UCS1_CMD_BASE_OFFSET + i - 1;
+		CAN_TxData.Command = UCS_DEV_LINK;
+		xQueueSendToBack(CanDataTx_qh, &CAN_TxData, 0);
+		HAL_Delay(75);
+	}
+}
+
+/*
+ * @brief Update devices' parameters.
+ */
+void DEV_UpdateParams() {
+
+	CAN_TxQueueData_t CAN_TxData = { 0 };
+
+	CAN_TxData.DevType		= AGU;
+	CAN_TxData.SPrefixCode	= UCS;
+
+	/* Waiting module available. */
+	HAL_Delay(50);
+
+	/* Set phase to all available devices. */
+//	CAN_TxData.CmdCode		= BCS_SETAmplitude;
+//	for (uint8_t i = 1; i <= MAX_UPCB_NUMBER; i++) {
+//		if (DEV_GetType()->DevType[i - 1] == SGU) {
+//			CAN_TxData.Address = UCS1_CMD_BASE_OFFSET + i - 1;
+//			CAN_TxData.Command = UCS_SET_AMPLITUDE;
+//			for(uint8_t j = 0; j < MAX_SGU_CHANNELS; j++) {
+//				CAN_TxData.Phase = DEV_GetGenData()[i - 1].Amplitude[j];
+//				xQueueSendToBack(CanDataTx_qh, &CAN_TxData, 0);
+//				HAL_Delay(75);
+//			}
+//		}
+//	}
+}
+
+/*
+ * @brief Device name decoder.
  **/
+void DEV_NameDecode(uint8_t State, uint32_t DevType, uint8_t DevSubtype, DEV_CharBuff_t* CharBuff) {
+
+	/* State decoding to char. */
+	switch (State) {
+		case DEV_OFF:
+			sprintf(&CharBuff->state[0], "OFF");
+			break;
+		case DEV_ON:
+			sprintf(&CharBuff->state[0], "ON");
+			break;
+		default:
+			sprintf(&CharBuff->state[0], "N.A.");
+			break;
+	}
+
+	/* Select the right type of addressed device. */
+	switch (DevType) {
+		case AGU:
+			sprintf(&CharBuff->dev_id[0], "AGU");
+			break;
+		default:
+			sprintf(&CharBuff->dev_id[0], "N.A.");
+			break;
+	}
+
+	/* Subtype decoding to char. */
+	switch (DevSubtype) {
+		case AGU0:
+			sprintf(&CharBuff->dev_subid[0], "AGU0");
+			break;
+		default:
+			sprintf(&CharBuff->dev_subid[0], "N.A.");
+			break;
+	}
+}
+
+/*
+ * @brief Convert decimal to state binary byte.
+ */
+uint8_t DEV_DecToStateByte(uint32_t DecValue) {
+
+	/* . */
+	uint8_t StateByte = 0;
+
+	/* Disable all if value is bigger than. */
+	if (DecValue > 99999999) {
+		return 0;
+	}
+	/* Conversion if it's ok. */
+	for (uint8_t i = 7; i > 0; i--) {
+		if ((DecValue % 10) > 0) {
+			StateByte |= 0x80;
+		}
+		DecValue /= 10;
+		StateByte >>= 1;
+	}
+	if (DecValue  > 0) {
+		StateByte |= 0x80;
+	}
+	return StateByte;
+}
+
+/*
+ * @brief Led channel control.
+ *
+ * @param channel: can have there values -	CH1, CH2, CH3, CH4, CH5, CH6, CH_ALL
+ * @param state: If state equal to 0, led is power off. If state above 0, led is power on.
+ *
+ */
+void LED_Ctrl(uint8_t channel, uint8_t state) {
+
+//	switch (channel) {
+//
+//	case CH1:
+//		if (state) { HAL_GPIO_WritePin(LED1_CH1_GPIO_Port, LED1_CH1_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED1_CH1_GPIO_Port, LED1_CH1_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH2:
+//		if (state) { HAL_GPIO_WritePin(LED2_CH2_GPIO_Port, LED2_CH2_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED2_CH2_GPIO_Port, LED2_CH2_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH3:
+//		if (state) { HAL_GPIO_WritePin(LED3_CH3_GPIO_Port, LED3_CH3_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED3_CH3_GPIO_Port, LED3_CH3_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH4:
+//		if (state) { HAL_GPIO_WritePin(LED4_CH4_GPIO_Port, LED4_CH4_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED4_CH4_GPIO_Port, LED4_CH4_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH5:
+//		if (state) { HAL_GPIO_WritePin(LED5_CH5_GPIO_Port, LED5_CH5_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED5_CH5_GPIO_Port, LED5_CH5_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH6:
+//		if (state) { HAL_GPIO_WritePin(LED6_CH6_GPIO_Port, LED6_CH6_Pin, GPIO_PIN_RESET); }
+//		else { HAL_GPIO_WritePin(LED6_CH6_GPIO_Port, LED6_CH6_Pin, GPIO_PIN_SET); }
+//		break;
+//	case CH_ALL:
+//		if (state) {
+//			HAL_GPIO_WritePin(LED1_CH1_GPIO_Port, LED1_CH1_Pin, GPIO_PIN_RESET);
+//			HAL_GPIO_WritePin(LED2_CH2_GPIO_Port, LED2_CH2_Pin, GPIO_PIN_RESET);
+//			HAL_GPIO_WritePin(LED3_CH3_GPIO_Port, LED3_CH3_Pin, GPIO_PIN_RESET);
+//			HAL_GPIO_WritePin(LED4_CH4_GPIO_Port, LED4_CH4_Pin, GPIO_PIN_RESET);
+//			HAL_GPIO_WritePin(LED5_CH5_GPIO_Port, LED5_CH5_Pin, GPIO_PIN_RESET);
+//			HAL_GPIO_WritePin(LED6_CH6_GPIO_Port, LED6_CH6_Pin, GPIO_PIN_RESET);
+//		} else {
+//			HAL_GPIO_WritePin(LED1_CH1_GPIO_Port, LED1_CH1_Pin, GPIO_PIN_SET);
+//			HAL_GPIO_WritePin(LED2_CH2_GPIO_Port, LED2_CH2_Pin, GPIO_PIN_SET);
+//			HAL_GPIO_WritePin(LED3_CH3_GPIO_Port, LED3_CH3_Pin, GPIO_PIN_SET);
+//			HAL_GPIO_WritePin(LED4_CH4_GPIO_Port, LED4_CH4_Pin, GPIO_PIN_SET);
+//			HAL_GPIO_WritePin(LED5_CH5_GPIO_Port, LED5_CH5_Pin, GPIO_PIN_SET);
+//			HAL_GPIO_WritePin(LED6_CH6_GPIO_Port, LED6_CH6_Pin, GPIO_PIN_SET);
+//		}
+//		break;
+//	default:
+//		break;
+//	}
+
+}
+
+/*
+ * @brief Sync pulse issue.
+ */
+void DEV_GiveSoftSyncPulse(void) {
+
+//	DEV_SyncInSel(1);
+//	HAL_GPIO_WritePin(MCU_SYNC_PULSE_GPIO_Port, MCU_SYNC_PULSE_Pin, GPIO_PIN_SET);
+//	HAL_Delay(1);
+//	HAL_GPIO_WritePin(MCU_SYNC_PULSE_GPIO_Port, MCU_SYNC_PULSE_Pin, GPIO_PIN_RESET);
+//	HAL_Delay(1);
+//	DEV_SyncInSel(0);
+
+}
+
+/*
+ * @brief Software button trigger.
+ */
 void BTN_SWPress(void) {
-	
+
 	/* Giving the semaphore. */
-	xSemaphoreGive(SWBtnPressed_SH);
-	/* Software triggered PA3 external interrupt input. It's like press a button. */
-	EXTI->SWIER = EXTI_SWIER_SWI3;
+	xSemaphoreGive(SoftBtnPressed_sh);
+	/* Software triggered external interrupt input. It's like press a button on the pin 3. */
+	EXTI->SWIER = EXTI_SWIER_SWIER3;
 }
-
-/* Transmitting functions. */
-
-/*
- * @brief
- *
- **/
-void SPI_Tx(uint8_t *pData, uint8_t size) {
-	
-	HAL_SPI_Transmit(&hspi1, pData, size, 10);
-}
-
-/*
- * @brief
- *
- **/
-void SPI_Rx(uint8_t *pData, uint8_t size) {
-	
-	HAL_SPI_Receive(&hspi1, pData, size, 10);
-}
-
-/*
- * @brief
- *
- **/
-void I2C_Tx(uint16_t address, uint16_t mem_address, uint16_t mem_addr_size, uint8_t *pData, uint16_t size) {
-	
-	HAL_I2C_Master_Transmit(&hi2c1, address, pData, size, 10);
-}
-
-/*
- * @brief
- *
- **/
-void I2C_Rx(uint16_t address, uint16_t mem_address, uint16_t mem_addr_size, uint8_t *pData, uint16_t size) {
-	
-	HAL_I2C_Master_Receive(&hi2c1, address, pData, size, 10);
-}
-
-
-/*
- * @brief
- *
- **/
--- COMMON/common.h
+++ COMMON/common.h
@@ -1,151 +1,326 @@
 /*
- * @brief Header file for common.c.
- * 
- * Created 08.19.21
+ * @brief Common.h
  *
- **/
+ * Created on: Jul 16, 2023
+ * Author: asw3005
+ *
+ */
 
 #ifndef COMMON_H_
 #define COMMON_H_
 
-#include "stm32f103xb.h"
+#include "stm32f4xx.h"
 
-#define MAX_ADDRESES_NUMBER		16
-#define LED_TIMEOUT				10000
+/* EEP data and constants. */
+
+/* Page's addresses. */
+#define EEP_DATA_ADDR			0x50
+#define	EEP_DATA_ADDR_SHIFTED	0xA0
+#define EEP_ID_PAGE				0x58
+#define EEP_ID_PAGE_SHIFTED		0xB0
 
 /*
- * @brief Broadcast command set.
- *
- **/
+	@brief EEP action constants.
+*/
 typedef enum {
 
-	CMD_DISABLE_DEVICE,
-	CMD_WRITE_VOLTAGE,
-	CMD_VIEW_ADDRESS,
-	CMD_RESERVED3,
-	CMD_RESERVED4,
-	CMD_RESERVED5,
-	CMD_RESERVED6,
-	CMD_RESERVED7
-	
-} CMD_BroadcastCommand;
+	EEP_WRITE_TO,
+	EEP_READ_FROM
+
+} EXT_MEM_RW_t;
 
 /*
- * @brief General command set.
- *
- **/
+ * @brief Page's codes.
+ */
+typedef enum
+{
+	ST_MANUFACTURER_CODE	= 0x00,
+	I2C_FAMILY_CODE			= 0x01,
+	MEMORY_DENSITY_CODE		= 0x02,
+	M24128_ERT				= 0x04
+
+} M24128_ID_PAGE_ADDR_t;
+
+/*
+ * @brief Internal memory organization struct.
+ */
+typedef	struct /*__attribute__((aligned(1), packed))*/ {
+	/* Memory address to select page and byte on the page. */
+	union {
+		uint16_t MemAddr;
+		struct {
+			uint8_t BYTE_ADDR;
+			uint8_t PAGE_ADDR;
+		};
+	};
+
+} M24128_MEM_DATA_t;
+
+/* */
+#define MPCB_DEFAULT_ADDRESS		0
+#define MPCB_EEP_CLEAR_ADDR			255
+#define MAX_MPCB_NUMBER				3
+#define MAX_UPCB_NUMBER				6
+#define MAX_SGU_CHANNELS			4
+#define NO_DEVICE_ON_PCB			-50
+#define LED_SCREEN_TIMEOUT			10000
+#define DEFAULT_CONNECTION_TIMEOUT	30000
+
+/*
+	@brief Device's command sets throw the UART.
+*/
 typedef enum {
 
-	CMD_GCS_IS_READY,
-	CMD_GCS_DISABLE_DEVICE,
-	CMD_GCS_WRITE_VOLTAGE,
-	CMD_GCS_READ_VOLTAGE,
-	CMD_GCS_SET_ADDRESS,
-	CMD_GCS_RESERVED5,
-	CMD_GCS_RESERVED6,
-	CMD_GCS_RESERVED7
-	
-} CMD_GeneralCommand;
+	/* No code received. */
+	NO_CODE_RECEIVED		= 0x00000000,
+	TYPE_UNDEFINED			= 0xFFFFFFFF,
+
+	/* Hardware reading request. */
+	MCS_HardwareReq			= 0x0801f858,
+	/* Command code to connect to the PCB. */
+	MCS_ConnectTo			= 0xD9F2D9F7,
+
+	/* Mother PCB commands (general command set). */
+	MCS_GETSysInfo			= 0x6135E130,
+	MCS_GETDeviceList		= 0x1019F501,
+	MCS_SETEthAddr			= 0xC4159A09,
+	MCS_SETAddr				= 0xAA582F78,
+	MCS_ENBip				= 0x7A984D73,
+	MCS_DISBip				= 0xFCA6393B,
+	MCS_GetParams			= 0xfdcc0d08,
+	MCS_SetParams			= 0xc2cd4d28,
+	MCS_SaveParams			= 0x5a25bd71,
+	MCS_RestoreParams		= 0x9f558a84,
+	MCS_SyncDevices			= 0x8792dc7c,
+
+	/* FPGA FIFO commands. */
+	FPGA_FifoReset			= 0xd38c60eb,
+	FPGA_GetSampleNumbers	= 0x46be02f9,
+	FPGA_GetFifo0samples	= 0xf61986a2,
+	FPGA_GetFifo1samples	= 0x83a7c015,
+	FPGA_GetTSampleNumbers	= 0x8ab39fa5,
+	FPGA_GetTSamples		= 0x56bc5e27,
+
+	/* FPGA configuration commands. */
+	FPGA_SoftwareStart		= 0xdf293ee9,
+	FPGA_SyncRegisters		= 0x2dfe7610,
+	FPGA_SetDefaulParams	= 0xfc0fedb3,
+	FPGA_SetCtrlReg 		= 0x865b5ba0,
+	FPGA_SetPeriodLimiter	= 0x7ae31452,
+	FPGA_SetFStartTim		= 0x6a175534,
+	FPGA_SetSStartTim		= 0xfe56a20a,
+	FPGA_SetFStopTim		= 0xec709233,
+	FPGA_SetSStopTim		= 0x0e55e89a,//
+	FPGA_SetThrStartHigh	= 0xea2e5b92,
+	FPGA_SetThrStartLow		= 0x213068ba,
+	FPGA_SetThr0Stop		= 0x280ba17b,
+	FPGA_SetThr1Stop		= 0x3fd89026,
+
+
+	/* Global command set. */
+	GCS_EnDisDevice			= 0x56b15d35,
+	GCS_VIEWAddr			= 0xE77CE3C0,
+
+	/* Broadcast command set. */
+	BCS_EnDisDevice			= 0xc5e30e37,
+	BCS_VIEWAddr			= 0x522652A6,
+
+
+	/* Unicast command set. */
+	UCS_GETInfo				= 0x8EE72275,
+	UCS_EnDisDevice			= 0xf019b7af,
+	UCS_SETAddr				= 0x827DBE96,
+	UCS_LOCKAddr			= 0xB8B37E88,
+
+	/* Command prefixes. */
+	FPGA					= 0x49e4e69d,
+	MCS						= 0x79823B50,
+	GCS						= 0x1700711E,
+	BCS						= 0x20415439,
+	UCS						= 0xEF8523B8,
+	AGU						= 0x08f37915,
+
+
+} ALL_CMD_SETS_t;
 
 /*
- * @brief  Data type external non volatile memory.
- *
- **/
+	Device subtype list.
+*/
+typedef enum {
+	/* Generator subtypes. */
+	AGU0,
+
+	/* No device available. */
+	NO_DEVICE_AVAILABLE = 0xFF
+
+
+} DEV_SUBTYPE_t;
+
+/*
+	@brief HVU states.
+*/
+typedef enum {
+
+	DEV_OFF,
+	DEV_ON,
+	STATE_UNDEFINED = 0xFF
+
+} DEV_STATE_t;
+
+/*
+ * @brief General constants.
+ */
+typedef enum {
+
+	/* MAX and MIN values for an external devices. */
+	SGU_MIN_AMPL			= 0,
+	SGU_MAX_AMPL			= 1024,
+
+	SGU_MIN_FREQ			= 1000000,
+	SGU_MAX_FREQ			= 250000000,
+
+	SGU_MIN_PHASE			= 0,
+	SGU_MAX_PHASE			= 360
+
+
+} DEV_CONST_t;
+
+/*
+ * @brief Location of user data in the EEP memory.
+ */
+typedef enum {
+	/* Address location. */
+	DEV_ADDR_BYTE 		= 0,
+	DEV_ADDR_PAGE 		= 0,
+
+	/* Generator data. */
+	GEN_DATA_BYTE 		= 0,
+	GEN_DATA_PAGE 		= 1
+
+} DEV_EXT_MEM_DATA_t;
+
+/*
+ * @brief Led channel definitions.
+ */
+typedef enum {
+
+	CH1 = 1,
+	CH2, CH3,
+	CH4, CH5,
+	CH6, CH_ALL
+
+} LED_CH_t;
+
+/*
+ * @brief
+ */
+typedef struct {
+	char state[5];
+	char dev_id[11];
+	char dev_subid[11];
+
+} DEV_CharBuff_t;
+
+/*
+ * @brief State of LED channels on the front panel.
+ */
+typedef struct {
+
+	uint8_t Channel;
+	uint8_t State;
+
+} LedChData_t;
+
+/*
+ * @brief Device list struct.
+ */
+typedef struct {
+	/* Type of devices. */
+	union {
+		uint32_t DevType[MAX_UPCB_NUMBER];
+		struct {
+			uint32_t Device_1;
+			uint32_t Device_2;
+			uint32_t Device_3;
+			uint32_t Device_4;
+			uint32_t Device_5;
+			uint32_t Device_6;
+		};
+	};
+	/* Subtype of devices. */
+	union {
+		uint8_t DevSubType[MAX_UPCB_NUMBER];
+		struct {
+			uint8_t SubDevice_1;
+			uint8_t SubDevice_2;
+			uint8_t SubDevice_3;
+			uint8_t SubDevice_4;
+			uint8_t SubDevice_5;
+			uint8_t SubDevice_6;
+		};
+	};
+
+} DEV_List_t;
+
+/*
+ * @brief Generators' data.
+ */
+typedef struct {
+	/* Generator's data. */
+	float Phase[4];
+	float Amplitude[4];
+	float Frequency[4];
+
+} DEV_GenData_t;
+
+/*
+ * @brief Define device electronic signature struct.
+ */
 typedef struct __attribute__((aligned(1), packed)) {
-	
-	uint8_t DeviceAddress;
-	
-} CMD_EPROMData;
 
-/*
- * @brief Queue data type.
- *
- **/
-typedef union __attribute__((aligned(1), packed)) {
-	
-	struct {
-		uint8_t CmdNumber;	
-		int16_t DacVoltage;
-	};
-	
-	struct {
-		uint8_t Command;	
-		uint8_t Data[8];
-	};
-	
-} CMD_QueueData_t;
+	union {
+		__IO uint32_t Id_0_31;
+		__IO uint32_t Id_XY_Coordinates;
+	} XYCoordinates;
 
-/*
- * @brief Device identificator and command data type.
- *
- **/
-typedef union __attribute__((aligned(1), packed)) {
-	uint16_t Identifier;
-	struct {
-		uint16_t ID_TODO		: 3;
-		uint16_t ID_ADDRESS		: 8;
-		uint16_t RESERVED15_11	: 5; 
-	};		
-} CMD_DevId_t;
+	union {
+		__IO uint32_t Id_32_63;
+		struct {
+			__IO uint32_t WAF_NUM_0_7 	: 8;
+			__IO uint32_t LOT_NUM_0_23 	: 24;
+		};
+	} WafLot;
 
-/*
- * @brief 32 bits filter bank register organization data type.
- *
- **/
-typedef union __attribute__((aligned(1), packed)) {
-	struct {
-		uint16_t FilterBankReg_LSB;
-		uint16_t FilterBankReg_MSB;
-	};
-	struct {
-		/* Must be zero. */
-		uint32_t RESERVED0	: 1;
-		uint32_t RTR		: 1;
-		uint32_t IDE		: 1;
-		/* Extended identifier. */
-		uint32_t EXID20_3	: 18;
-		/* Standart identifier. */
-		uint32_t STID31_21	: 11;		
-	};
-	
-} CAN_FilterBankRegOrg32_t;
+	union {
+		__IO uint32_t Id_64_95;
+		__IO uint32_t Id_LotNum_24_55;
+	} LotNum;
 
-/*
- * @brief 16 bits filter bank register organization data type.
- *
- **/
-typedef union __attribute__((aligned(1), packed)) {
-	
-	uint16_t FilterBankReg;
+} STM32DevId_t;
 
-	struct {
-		/* Extended identifier. */
-		uint16_t EXID18_16	: 3;
-		/* A dominant single identifier extension (IDE) bit means that a standard CAN identifier with no extension is being
-		 * transmitted. */
-		uint16_t IDE		: 1;
-		/* The single remote transmission request bit is dominant when information is required from another node. */
-		uint16_t RTR		: 1;
-		/* Standart identifier. */
-		uint16_t STID31_21	: 11;		
-	};
-	
-} CAN_FilterBankRegOrg16_t;
-
+/* Electronic device signature on the memory MAP. */
+#define STM32_EL_DEV_SIG  ((STM32DevId_t *) UID_BASE)
+/* Flash size. */
+#define STM32_FLASH_SIZE ((uint16_t *) FLASHSIZE_BASE)
 
 /* Public function prototypes. */
+uint32_t* MCS_MakeCmdCodes(void);
+DEV_List_t* DEV_GetType(void);
+DEV_GenData_t* DEV_GetGenData(void);
+uint8_t DEV_DecToStateByte(uint32_t DecValue);
+void DEV_WriteDevType(uint8_t dev_channel, uint32_t dev_type, uint8_t dev_subtype);
+void DEV_NameDecode(uint8_t State, uint32_t DevType, uint8_t DevSubtype, DEV_CharBuff_t* CharBuff);
 
-void CAN_ConfigAddr(CAN_HandleTypeDef *hcan, const uint8_t bank, const uint8_t address, const uint8_t cmd_1,
-					const uint8_t cmd_2,  const uint8_t cmd_3,  const uint8_t cmd_4);
-void CAN_ConfigBank(CAN_HandleTypeDef *hcan);
+void DEV_UpdateList();
+void DEV_UpdateParams();
+void DEV_UpdateState(uint32_t dev_type, uint32_t sprefix, uint8_t address);
+
+
+void DEV_RWGenEepAll(uint8_t ReadWrite);
+
+void DEV_GiveSoftSyncPulse(void);
 
 void BTN_SWPress(void);
-
-void SPI_Tx(uint8_t *pData, uint8_t size);
-void SPI_Rx(uint8_t *pData, uint8_t size);
-
-void I2C_Tx(uint16_t address, uint16_t mem_address, uint16_t mem_addr_size, uint8_t *pData, uint16_t size);
-void I2C_Rx(uint16_t address, uint16_t mem_address, uint16_t mem_addr_size, uint8_t *pData, uint16_t size);
-
+void LED_Ctrl(uint8_t channel, uint8_t state);
 
 #endif /* COMMON_H_ */
